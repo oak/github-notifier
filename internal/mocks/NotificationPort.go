5aaab5e6ba69889f02ddfb1bc8c8b7c0ec17527b
Alex DC <dev@oak3.dev>
2026-02-08 14:17:18 +0100

    test: missing mock

--- internal/mocks/NotificationPort.go
+++ internal/mocks/NotificationPort.go
@@ -31,6 +31,24 @@ func (_m *NotificationPort) NotifyNewPullRequests(title string, prs []*pullreque
 	return r0
 }
 
+// SupportsClickActions provides a mock function with no fields
+func (_m *NotificationPort) SupportsClickActions() bool {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for SupportsClickActions")
+	}
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func() bool); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	return r0
+}
+
 // NewNotificationPort creates a new instance of NotificationPort. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewNotificationPort(t interface {
