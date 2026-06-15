13d4d638d599f2f70c0c6224644bd18e05b4ccf0
Alex <dev@oak3.dev>
2026-06-15 10:03:45 +0200

    fix: app icons

--- assets/embed.go
+++ assets/embed.go
@@ -4,10 +4,10 @@ import _ "embed"
 
 // Embedded icon files for the system tray
 
-//go:embed icon.png
+//go:embed icon.svg
 var DarkIcon []byte
 
-//go:embed icon_light.png
+//go:embed icon_light.svg
 var LightIcon []byte
 
 //go:embed git-pull-request.svg
